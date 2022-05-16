ae6f1020937bad607162814119eba772cec88836
oliver <56241978+oliverscode@users.noreply.github.com>
2022-05-16 21:19:13 +0800

    Update rollup.config.js
    add host

--- rollup.config.js
+++ rollup.config.js
@@ -15,39 +15,40 @@ const OUT_DIR = "build";
 const OUT_FILE = `${OUT_DIR}/index.html`;
 
 const hashStatic = () => ({
-  name: "hash-static",
-  buildStart: () => rimraf.sync(OUT_DIR),
-  writeBundle: () => {
-    posthtml([hash({ path: OUT_DIR })])
-      .process(fs.readFileSync(OUT_FILE, "utf-8"))
-      .then((result) => fs.writeFileSync(OUT_FILE, result.html));
-  },
+    name: "hash-static",
+    buildStart: () => rimraf.sync(OUT_DIR),
+    writeBundle: () => {
+        posthtml([hash({ path: OUT_DIR })])
+            .process(fs.readFileSync(OUT_FILE, "utf-8"))
+            .then((result) => fs.writeFileSync(OUT_FILE, result.html));
+    },
 });
 
 export default {
-  input: "src/index.js",
-  output: {
-    sourcemap: !PROD,
-    format: "iife",
-    name: "app",
-    file: `${OUT_DIR}/bundle.[hash].js`,
-  },
-  plugins: [
-    copy({ targets: [{ src: "public/*", dest: OUT_DIR }] }),
-    svelte({
-      compilerOptions: {
-        dev: !PROD,
-      },
-    }),
-    css({ output: "bundle.[hash].css" }),
-    resolve(),
-    !PROD &&
-      serve({
-        contentBase: [OUT_DIR],
-        port: 3000,
-      }),
-    !PROD && livereload({ watch: OUT_DIR }),
-    PROD && terser(),
-    PROD && hashStatic(),
-  ],
+    input: "src/index.js",
+    output: {
+        sourcemap: !PROD,
+        format: "iife",
+        name: "app",
+        file: `${OUT_DIR}/bundle.[hash].js`,
+    },
+    plugins: [
+        copy({ targets: [{ src: "public/*", dest: OUT_DIR }] }),
+        svelte({
+            compilerOptions: {
+                dev: !PROD,
+            },
+        }),
+        css({ output: "bundle.[hash].css" }),
+        resolve(),
+        !PROD &&
+            serve({
+                contentBase: [OUT_DIR],
+                port: 3000,
+                host: "0.0.0.0",
+            }),
+        !PROD && livereload({ watch: OUT_DIR }),
+        PROD && terser(),
+        PROD && hashStatic(),
+    ],
 };
